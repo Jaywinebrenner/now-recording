a6429071f8e22c6edfd9b1d05f26ca5356e81677
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-05-06 09:17:31 -0700

    add basic strucutre

--- pages/about.js
+++ pages/about.js
@@ -2,18 +2,25 @@
 import React, {useState, useEffect} from 'react';
 import Nav from './components/nav'
 import Layout from './components/layout'
+import MapContainer from './components/map-container';
+
 
 export default function About() {
+  // const {isLoaded} = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY, })
+
+
 
 
   return (
     <Layout>
         <div className="about">
-          {/* <Nav/> */}
+   
           <div className="about__wrapper">
             <h1>NOW! Recording Studio is located in the heart of St Johns in a 3000 Square foot loft space overlooking the so-and-so train tracks with a view of Mount St. Helens.</h1>
             <h1>Helmed by long time music vet Mitch Dries.</h1>
           </div>
+          <MapContainer></MapContainer>
+
       </div>
     </Layout>
   )
---
+++ pages/components/map-container.js
@@ -0,0 +1,71 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, {Component} from "react";
+
+
+const containerStyle = {
+    position: 'relative',  
+    width: '300px',
+    height: '300px',
+    left: '0',
+    right: '0',
+    marginLeft: 'auto',
+    marginRight: "auto",
+    position: "absolute",
+    width: "40%",
+  }
+ 
+//note: code formatted for ES6 here
+export class MapContainer extends Component {
+    state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+   
+    onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+   
+    onMapClicked = (props) => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
+    };
+   
+    render() {
+      return (
+        <Map
+        class="map"
+        google={this.props.google}
+        initialCenter={{
+          lat: 45.58582,
+          lng: -122.77288
+        }}
+        style={containerStyle}
+        zoom={15}
+        onClick={this.onMapClicked}
+      >
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+   
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      )
+    }
+  }
+ 
+export default GoogleApiWrapper({
+  apiKey: (process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY)
+})(MapContainer)
